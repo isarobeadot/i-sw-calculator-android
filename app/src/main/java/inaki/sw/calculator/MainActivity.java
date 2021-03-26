132664c86dff9e0f55e38051010102143261d4da
Iñaki Sarobe Adot <inaki.sarobe.a@gmail.com>
2021-03-26 15:28:40 +0100

    Removed operation TextView's padding

--- app/src/main/java/inaki/sw/calculator/MainActivity.java
+++ app/src/main/java/inaki/sw/calculator/MainActivity.java
@@ -13,7 +13,6 @@ import android.text.Html;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
